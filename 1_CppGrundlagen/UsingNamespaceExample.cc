0cc73c8403486f7d59c766b6e7a0f236ba23a58f
franneck94 <jan.schaffranek@rub.de>
2020-08-16 20:22:55 +0200

    Up

---
+++ 1_CppGrundlagen/UsingNamespaceExample.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//using namespace std;
+
+using std::cout;
+using std::endl;
+
+//int cout(int a)
+//{
+//	return a;
+//}
+
+int main()
+{
+	cout << "Hello World!" << endl;
+
+	return 0;
+}
--- 6_Programmieraufgabe/main.cc
+++ 6_Programmieraufgabe/main.cc
@@ -10,31 +10,23 @@
 
 #include "exercise.h"
 
-using std::cout;
-using std::endl;
-using std::cin;
-using std::string;
-using std::vector;
-using std::tuple;
-using std::map;
-
 int main()
 {
 	// Aufgabe 1
 	Friends friends;
 
-	friends["Jan"] = pair<int, int>(23, 68);
-	friends["Dennis"] = pair<int, int>(27, 75);
-	friends["Peter"] = pair<int, int>(54, 70);
-	friends["Kathrin"] = pair<int, int>(30, 60);
-	friends["Lisa"] = pair<int, int>(26, 65);
+	friends["Jan"] = std::pair<int, int>(23, 68);
+	friends["Dennis"] = std::pair<int, int>(27, 75);
+	friends["Peter"] = std::pair<int, int>(54, 70);
+	friends["Kathrin"] = std::pair<int, int>(30, 60);
+	friends["Lisa"] = std::pair<int, int>(26, 65);
 
 	// Aufgabe 2
-	string oldest_friend = get_oldest_friend(friends);
-	string heaviest_friend = get_heaviest_friend(friends);
+	std::string oldest_friend = get_oldest_friend(friends);
+	std::string heaviest_friend = get_heaviest_friend(friends);
 
-	cout << "My oldest friend: " << oldest_friend << endl;
-	cout << "My heaviest friend: " << heaviest_friend << endl;
+	std::cout << "My oldest friend: " << oldest_friend << std::endl;
+	std::cout << "My heaviest friend: " << heaviest_friend << std::endl;
 
 	return 0;
 }
