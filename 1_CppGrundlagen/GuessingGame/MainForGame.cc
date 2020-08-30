ef537d0bd7a3bd59bb9a94368b8bcb75b94291f7
Jan Schaffranek <jan.schaffranek@rub.de>
2020-08-30 11:21:56 +0200

    Up

--- 1_CppGrundlagen/GuessingGame/MainForGame.cc
+++ 1_CppGrundlagen/GuessingGame/MainForGame.cc
@@ -6,9 +6,9 @@
  using std::endl;
  using std::cin;
 
- // Bevor eine Funktion aufgerufen werden kann, muss sie zumindest
- // vorher deklariert sein. Zeitgleich muss sie aber an anderer Stelle
- // im Code auch definiert sein.
+//  Bevor eine Funktion aufgerufen werden kann, muss sie zumindest
+//  vorher deklariert sein. Zeitgleich muss sie aber an anderer Stelle
+//  im Code auch definiert sein.
 
  int main()
  {
