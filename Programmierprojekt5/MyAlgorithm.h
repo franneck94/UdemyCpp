820c3d0d4bef9616481da2765b98026d6d553ab4
Jan Schaffranek <jan.schaffranek@rub.de>
2020-10-02 16:29:27 +0200

    Up

--- Programmierprojekt5/MyAlgorithm.h
+++ Programmierprojekt5/MyAlgorithm.h
@@ -7,7 +7,7 @@
 namespace cppmath
 {
 
-template <class InputIterator>
+template <class InputIterator, std::enable_if<is_iterator<InputIterator>::value>>
 auto sum(InputIterator itBegin, InputIterator itEnd)
 {
     auto sum_value{ 0 };
@@ -20,56 +20,56 @@ auto sum(InputIterator itBegin, InputIterator itEnd)
     return sum_value;
 }
 
-// template <class Container, std::enable_if<is_cpp_container<Container>::value>>
-// double mean(Container &container)
-// {
-//     return sum(container) / container.size();
-// }
+template <class Container, std::enable_if<is_container<Container>::value>>
+double mean(Container &container)
+{
+    return sum(container) / container.size();
+}
 
-// template <class Container, std::enable_if<is_cpp_container<Container>::value>>
-// double median(Container &container)
-// {
-//     double median_value = 0.0;
-//     bool has_odd_length = container.size() % 2;
+template <class Container, std::enable_if<is_container<Container>::value>>
+double median(Container &container)
+{
+    double median_value = 0.0;
+    bool has_odd_length = container.size() % 2;
 
-//     if (has_odd_length)
-//     {
-//         std::size_t index = container.size() / 2;
-//         median_value = container[index];
-//     }
-//     else
-//     {
-//         std::size_t index1 = container.size() / 2;
-//         std::size_t index2 = index1 - 1;
-//         median_value = (container[index1] + container[index2]) / 2.0;
-//     }
+    if (has_odd_length)
+    {
+        std::size_t index = container.size() / 2;
+        median_value = container[index];
+    }
+    else
+    {
+        std::size_t index1 = container.size() / 2;
+        std::size_t index2 = index1 - 1;
+        median_value = (container[index1] + container[index2]) / 2.0;
+    }
 
-//     return median_value;
-// }
+    return median_value;
+}
 
-// template <class Container, std::enable_if<is_cpp_container<Container>::value>>
-// double variance(Container &container)
-// {
-//     double mean_value = mean(container);
-//     double variance_value = 0.0;
-//     double probability = 1.0 / (double)container.size();
+template <class Container, std::enable_if<is_container<Container>::value>>
+double variance(Container &container)
+{
+    double mean_value = mean(container);
+    double variance_value = 0.0;
+    double probability = 1.0 / (double)container.size();
 
-//     for (std::size_t i = 0; i < container.size(); ++i)
-//     {
-//         double x_i = container[i];
-//         variance_value += pow(x_i - mean_value, 2.0) * probability;
-//     }
+    for (std::size_t i = 0; i < container.size(); ++i)
+    {
+        double x_i = container[i];
+        variance_value += pow(x_i - mean_value, 2.0) * probability;
+    }
 
-//     return variance_value;
-// }
+    return variance_value;
+}
 
-// template <class Container, std::enable_if<is_cpp_container<Container>::value>>
-// double stddev(Container &container)
-// {
-//     double variance_value = variance(container);
-//     double stddev_value = sqrt(variance_value);
+template <class Container, std::enable_if<is_container<Container>::value>>
+double stddev(Container &container)
+{
+    double variance_value = variance(container);
+    double stddev_value = sqrt(variance_value);
 
-//     return stddev_value;
-// }
+    return stddev_value;
+}
 
 } // end namespace
--- Programmierprojekt5/MyTypeTraits.h
+++ Programmierprojekt5/MyTypeTraits.h
@@ -3,17 +3,27 @@
 
 #include "MyVector.h"
 #include "MyArray.h"
+#include "MyIterators.h"
 
 namespace cppmath
 {
 
 template <typename T>
-struct is_cpp_container : public std::false_type {};
+struct is_container : public std::false_type {};
 
 template<class T, class Alloc>
-struct is_cpp_container<cppmath::Vector<T, Alloc>> : public std::true_type {};
+struct is_container<cppmath::Vector<T, Alloc>> : public std::true_type {};
 
 template<class T, std::size_t N>
-struct is_cpp_container<cppmath::Array<T, N>> : public std::true_type {};
+struct is_container<cppmath::Array<T, N>> : public std::true_type {};
+
+template <typename T>
+struct is_iterator : public std::false_type {};
+
+template<class T>
+struct is_iterator<cppmath::RandomAcessIterator<T>> : public std::true_type {};
+
+template<class T>
+struct is_iterator<cppmath::ReverseRandomAcessIterator<T>> : public std::true_type {};
 
 } // namespace cppmath
