83ddfdea0454cf4c6446b8493809eba513e26daf
franneck94 <jan.schaffranek@rub.de>
2020-12-09 18:25:34 +0100

    Updated debugger video

---
+++ 1_CppGrundlagen/DebugGame/MainForGame.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "Spiel.h"
+
+// Bevor eine Funktion aufgerufen werden kann, muss sie zumindest
+// vorher deklariert sein. Zeitgleich muss sie aber an anderer Stelle
+// im Code auch definiert sein.
+
+int main()
+{
+    // Aufruf
+    game();
+
+    return 0;
+}
--- 1_CppGrundlagen/Modulo.cc
+++ 1_CppGrundlagen/Modulo.cc
@@ -2,23 +2,19 @@
 
 int modulo(int n1, int n2)
 {
-    // 10 / 3 = 3
-    int step1 = n1 / n2;
-    // rest = 10 - 3*3 = 1
-    int step2 = n1 - (n2 * step1);
+    int result_integer_division = n1 / n2;
+    int remainder = n1 - (n2 * result_integer_division);
 
-    return step2;
+    return remainder;
 }
 
 int main()
 {
     int a = 1337;
     int b = 17;
-    int m;
 
-    m = modulo(a, b);
-
-    std::cout << m << std::endl;
+    std::cout << modulo(a, b) << std::endl;
+    std::cout << a % b << std::endl;
 
     return 0;
 }
