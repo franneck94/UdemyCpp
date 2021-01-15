12a2d2ee1df49ec68a0f155882edb63f0bbdafb4
franneck94 <schaffranek.jan@web.de>
2021-01-15 14:01:32 +0100

    up

--- GUI-Computerspiel/Helper.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "mainwindow.h"
-
-unsigned int random_uint(const unsigned int &lbound, const unsigned int &ubound);
-
-Position random_position(const unsigned int &lbound1, const unsigned int &ubound1, const unsigned int &lbound2, const unsigned int &ubound2);
