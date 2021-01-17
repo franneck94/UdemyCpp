3c3ad8f2adc01694f63b03b493e3deffccc409f7
Jan S <schaffranek.jan@web.de>
2021-01-17 12:21:32 +0100

    up

--- 14_AdvancedCppFeatures/Cpp20_Ranges.cc
+++ 14_AdvancedCppFeatures/Cpp20_Ranges.cc
@@ -12,15 +12,21 @@ auto print = [](auto const& view) {
 
 int main()
 {
+    // Drop, Take, Transform,
     std::vector numbers1{1, 2, 3, 4, 5, 6};
     auto results1 = std::views::reverse(numbers1)
         | std::views::drop(2)
         | std::views::take(2)
-        | std::views::transform([](const int n){return n * 2;});
+        | std::views::transform([](const int n){return n * 2;})
+        | std::views::filter([](int n){ return n % 2 == 0; });
 
     for (auto v: results1) std::cout << v << " ";
     std::cout << std::endl;
 
+    for (auto v: numbers1) std::cout << v << " ";
+    std::cout << std::endl;
+
+    // Filter, Transform
     std::vector<int> numbers2{1, 2, 3, 4, 5, 6};
     auto results2 = numbers2
         | std::views::filter([](int n){ return n % 2 == 0; })
@@ -29,6 +35,7 @@ int main()
     for (auto v: results2) std::cout << v << " ";
     std::cout << std::endl;
 
+    // Split
     constexpr std::string_view hello { "Hello C++ 20 !" };
     auto res = hello | std::ranges::views::split(' ');
     std::cout << "substrings: ";
