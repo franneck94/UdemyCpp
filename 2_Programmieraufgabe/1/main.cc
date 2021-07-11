776fd8f2ee60f9665090d837a77e72a24b363900
franneck94 <schaffranek.jan@web.de>
2021-07-11 10:59:50 +0200

    up

--- 2_Programmieraufgabe/1/main.cc
+++ 2_Programmieraufgabe/1/main.cc
@@ -1,6 +1,6 @@
-#include "exercise.h"
 #include <iostream>
 
+#include "exercise.h"
 
 int main()
 {
@@ -8,32 +8,31 @@ int main()
     const unsigned int array_length = 100;
     double sum = 0.0;
 
-    double *p_array_exercise1 = nullptr;
-    p_array_exercise1 = new double[array_length];
+    double *p_array1 = nullptr;
+    p_array1 = new double[array_length];
 
     for (unsigned int i = 0; i < 100; i++)
     {
-        p_array_exercise1[i] = i;
+        p_array1[i] = i;
     }
 
-    sum = array_sum(p_array_exercise1, array_length);
+    sum = array_sum(p_array1, array_length);
     std::cout << "(Exercise 1) Array Sum = " << sum << std::endl;
 
     // Exercise 2
     unsigned int input_array_length = 0;
     int input_array_value = 0;
 
-    std::cout << std::endl << "Gebe Array Length ein!" << std::endl;
+    std::cout << std::endl << "Enter the array length: " << std::endl;
     std::cin >> input_array_length;
-    std::cout << std::endl << "Gebe Array Wert ein!" << std::endl;
+    std::cout << std::endl << "Enter the array value: " << std::endl;
     std::cin >> input_array_value;
 
-    int *p_array_exercise2 =
-        array_constructor(input_array_value, input_array_length);
+    int *p_array2 = array_constructor(input_array_value, input_array_length);
 
     for (unsigned int i = 0; i < input_array_length; i++)
     {
-        std::cout << p_array_exercise2[i] << std::endl;
+        std::cout << p_array2[i] << std::endl;
     }
 
     return 0;
