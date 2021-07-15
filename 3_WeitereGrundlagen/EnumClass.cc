35d05a3abe7ca68b55c93392ee55fcf1f3037495
franneck94 <schaffranek.jan@web.de>
2021-07-15 17:43:10 +0200

    up

--- 3_WeitereGrundlagen/EnumClass.cc
+++ 3_WeitereGrundlagen/EnumClass.cc
@@ -1,65 +1,30 @@
 #include <iostream>
 
-// 1.) + 2.)
-enum Colors
-{
-    RED = 0,
-    GREEN = 1,
-    BLUE = 2,
-};
-
-// unsigned int RED = 42;
-
-// 1.) + 2.)
-enum Colors2
-{
-    RED2 = 0,
-    GREEN2 = 1,
-    BLUE2 = 2,
-};
-
-// 3.)
-enum class Colors3 : unsigned char
-{
-    RED = 0,
-    GREEN = 1,
-    BLUE = 2,
-};
-
-enum class Colors4 : unsigned char
-{
-    RED = 0,
-    GREEN = 1,
-    BLUE = 2,
-};
-
 // 1.) Two Enums cannot share the same names
-// 2.) No variable can have a name which is already in some enumeration
+// 2.) No varaible can have a name which already reserved by an enum
 // 3.) Enums are not type safe
+
+enum class PermissionLevel
+{
+    STUDENT = 1,
+    TUTOR = 2,
+    INSTRUCTOR = 3,
+    ADMIN = 4
+};
+
+enum class ExamPerson
+{
+    STUDENT,
+    INSTRUCTOR
+};
+
 int main()
 {
-    // 1.) + 2.)
-    enum Colors c1 = Colors::RED;
+    PermissionLevel permission_level_jan = PermissionLevel::INSTRUCTOR;
+    PermissionLevel permission_level_peter = PermissionLevel::STUDENT;
 
-    enum Colors2 c2 = Colors2::RED2;
-
-    // 3.)
-    double val = 42.42;
-
-    if (val == c1)
-    {
-        std::cout << "Equal" << std::endl;
-    }
-
-    Colors3 c3 = Colors3::RED;
-    Colors3 c4 = Colors3::RED;
-
-    unsigned char a = 'a';
-
-    if (a == static_cast<unsigned char>(c3))
-    {
-        std::cout << "Equal" << std::endl;
-    }
+    ExamPerson jan = ExamPerson::INSTRUCTOR;
+    ExamPerson peter = ExamPerson::STUDENT;
 
     return 0;
 }
