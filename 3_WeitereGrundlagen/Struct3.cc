0d9b7f3008f6a387299c70097b84c1d2bc3842c0
franneck94 <schaffranek.jan@web.de>
2021-07-16 14:56:44 +0200

    up

--- 3_WeitereGrundlagen/Struct3.cc
+++ 3_WeitereGrundlagen/Struct3.cc
@@ -9,21 +9,21 @@ enum class Lanes : int
     LEFT_LANE = -1,
 };
 
-typedef struct Vehicle
+struct Vehicle
 {
     int id;
     Lanes lane;
     float velocity;
-} TS_Vehicle;
+};
 
-void print_vehicle_data(const TS_Vehicle &v)
+void print_vehicle_data(const Vehicle &v)
 {
     std::cout << "Vehicle ID: " << v.id << std::endl;
     std::cout << "Vehicle Lane: " << static_cast<int>(v.lane) << std::endl;
     std::cout << "Vehicle Vel: " << v.velocity << std::endl;
 }
 
-void print_vehicles(const TS_Vehicle *vehicles)
+void print_vehicles(const Vehicle *vehicles)
 {
     for (int i = 0; i < NUM_VEHICLES; i++)
     {
@@ -33,19 +33,19 @@ void print_vehicles(const TS_Vehicle *vehicles)
 
 int main()
 {
-    TS_Vehicle v1 = {1, Lanes::CENTER_LANE, 100.0f};
-    TS_Vehicle v2 = {2, Lanes::RIGHT_LANE, 100.0f};
-    TS_Vehicle v3 = {3, Lanes::LEFT_LANE, 100.0f};
+    Vehicle v1 = {1, Lanes::CENTER_LANE, 100.0f};
+    Vehicle v2 = {2, Lanes::RIGHT_LANE, 100.0f};
+    Vehicle v3 = {3, Lanes::LEFT_LANE, 100.0f};
 
     // dtype varName[numElements]
-    TS_Vehicle vehicles[NUM_VEHICLES];
+    Vehicle vehicles[NUM_VEHICLES];
     vehicles[0] = v1;
     vehicles[1] = v2;
     vehicles[2] = v3;
 
     print_vehicles(vehicles);
 
-    TS_Vehicle *v4 = new TS_Vehicle{4, Lanes::LEFT_LANE, 100.0f};
+    Vehicle *v4 = new Vehicle{4, Lanes::LEFT_LANE, 100.0f};
     print_vehicle_data(*v4);
 
     return 0;
