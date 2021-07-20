8830ef1262fafa6c5a66f4f1b5f8d1fe510f598d
franneck94 <schaffranek.jan@web.de>
2021-07-20 12:34:53 +0200

    up

--- 2_ArrayGrundlagen/References2.cc
+++ 2_ArrayGrundlagen/References2.cc
@@ -12,19 +12,25 @@ void f2(int &number)
     number++;
 }
 
+// CallByValue
 int f3(int number)
 {
-    return number + 1;
+    number++;
+
+    return number;
 }
 
 int main()
 {
-    int num = 5;
+    int num = 0;
     std::cout << num << std::endl;
+
     f1(num);
     std::cout << num << std::endl;
+
     f2(num);
     std::cout << num << std::endl;
+
     num = f3(num);
     std::cout << num << std::endl;
 
