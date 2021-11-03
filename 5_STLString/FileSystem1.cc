b824d8494efdd9f75895a9e53932d1ef3793a6e5
franneck94 <schaffranek.jan@web.de>
2021-11-03 18:41:14 +0100

    added

---
+++ 5_STLString/FileSystem1.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+int main()
+{
+    fs::path workspace_path = "C:/Users/Jan/Dropbox/_Coding/UdemyCpp";
+    fs::path chapter_path = workspace_path;
+    chapter_path /= "5_STLString";
+    fs::path currentfile_path = fs::current_path();
+    currentfile_path /= "FileSystem1.cc";
+
+    std::cout << "workspace_path = " << workspace_path << '\n';
+    std::cout << "chapter_path = " << chapter_path << '\n';
+    std::cout << "currentfile_path = " << currentfile_path << '\n';
+
+    std::cout << "relative_path " << currentfile_path.relative_path() << std::endl;
+    std::cout << "parent_path " << currentfile_path.parent_path() << std::endl;
+    std::cout << "filename " << currentfile_path.filename() << std::endl;
+    std::cout << "stem " << currentfile_path.stem() << std::endl;
+    std::cout << "extension " << currentfile_path.extension() << std::endl;
+
+    std::cout << currentfile_path << " exists: " << std::boolalpha << fs::exists(currentfile_path) << std::endl;
+    std::cout << currentfile_path << " is file: " << std::boolalpha << fs::is_regular_file(currentfile_path) << std::endl;
+    std::cout << currentfile_path << " is directory: " << std::boolalpha << fs::is_directory(currentfile_path) << std::endl;
+}
---
+++ 5_STLString/FileSystem2.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+int main()
+{
+    fs::path chapter_path = fs::current_path();
+    fs::path currentfile_path = fs::current_path();
+    currentfile_path /= "FileSystem2.cc";
+
+    for (auto it = fs::directory_iterator(chapter_path); it != fs::directory_iterator(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    fs::path new_directory_path;
+    new_directory_path /= fs::current_path();
+    new_directory_path /= "test";
+    if (!fs::exists(new_directory_path))
+    {
+        fs::create_directory(new_directory_path);
+    }
+    fs::path destionation_path;
+    destionation_path /= new_directory_path;
+    destionation_path /= "FileSystemCopy.cc";
+    destionation_path.replace_extension("cpp");
+    if (!fs::exists(destionation_path))
+    {
+        fs::copy_file(currentfile_path, destionation_path);
+    }
+    std::system("ls -l test");
+    std::system("tree test");
+    fs::remove_all("test");
+}
