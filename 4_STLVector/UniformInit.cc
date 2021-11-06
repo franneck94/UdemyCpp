9612fc34e2359c3b12d7ce730d16c202cd0f3014
franneck94 <schaffranek.jan@web.de>
2021-11-06 16:37:30 +0100

    up

--- 4_STLVector/UniformInit.cc
+++ 4_STLVector/UniformInit.cc
@@ -2,7 +2,19 @@
 
 int main()
 {
+    auto i = 3.5;
+    auto j = static_cast<int>(i);
 
+    auto k = double{0.0F};
+    auto l{0.0F};
+    auto m{0};
+    auto n{0U};
+    auto o{0L};
+
+    auto p = int{1};
+    int q{1};
+
+    std::cout << i << j << k << l << m << n << o << p << q << std::endl;
 
     return 0;
 }
--- 6_STLContainerIterator/StructuredBindings.cc
+++ 6_STLContainerIterator/StructuredBindings.cc
@@ -3,6 +3,13 @@
 #include <string>
 #include <tuple>
 
+void get_data_old(int &a, std::string &b, double &c)
+{
+    a = 1337;
+    b = "data";
+    c = 1337.0;
+}
+
 std::tuple<int, std::string, double> get_data()
 {
     return std ::make_tuple(1337, "data", 1337.0);
@@ -10,12 +17,17 @@ std::tuple<int, std::string, double> get_data()
 
 int main()
 {
-    std::map<int, std::string> m;
+    int a;
+    std::string b;
+    double c;
 
-    const auto [it, inserted] = m.insert({1, "one"});
-    std::cout << "inserted = " << inserted << '\n' << "value = " << it->second << '\n';
+    get_data_old(a, b, c);
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
 
-    const auto &[id, name, score] = get_data();
-    // id = 2;
+    const auto [id, name, score] = get_data();
     std::cout << id << std::endl;
+    std::cout << name << std::endl;
+    std::cout << score << std::endl;
 }
