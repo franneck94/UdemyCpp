dc7d23cb9901f718acc78b080f1ac46c84852d8f
franneck94 <schaffranek.jan@web.de>
2022-03-17 14:25:10 +0100

    up

--- 02_ArrayPointer/2_2_Exercise/Exercise/exercise.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Exercise 1
-void push_back(int *&input_array, const unsigned int &size, const int &value);
-
-// Exercise 2
-void pop_back(int *&input_array, const unsigned int &size);
