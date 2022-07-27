4a4361d3887087c6e49150f26275f1065ebf45cf
franneck94 <schaffranek.jan@web.de>
2022-07-27 15:58:37 +0200

    up

--- 10_Lambda/Function.cc
+++ 10_Lambda/Function.cc
@@ -1,19 +1,14 @@
 #include <functional>
 #include <iostream>
 
-void f1(int arg)
-{
-    std::cout << arg << '\n';
-}
-
-int f2(int arg)
+int f(int arg)
 {
     std::cout << arg << '\n';
 
     return ++arg;
 }
 
-int func3(std::function<int(int)> func)
+int new_approach2(std::function<int(int)> func)
 {
     return func(2);
 }
@@ -22,12 +17,16 @@ int main()
 {
     auto param = int{1};
 
-    auto func1 = std::function{f1};
-    func1(param);
-    auto func2 = std::function{f2};
-    auto result = func2(param);
+    // Old C-style approach
+    int (*old_approach)(int);
+    old_approach = f;
+    old_approach(2);
 
-    func3(f2);
+    // C++ approach
+    auto new_approach = std::function{f};
+    auto result = new_approach(param);
+
+    new_approach2(f);
 
     return 0;
 }
