7545906ac9b0e14f307cc7d66f75ce65699c08d1
franneck94 <schaffranek.jan@web.de>
2022-07-27 16:28:18 +0200

    up

--- 12_SmartPointer/WeakPointer.cc
+++ 12_SmartPointer/WeakPointer.cc
@@ -5,31 +5,29 @@
 class ScopeTest
 {
 public:
-    ScopeTest(const std::string &name) : m_name(name)
+    ScopeTest(int val) : m_val(val)
     {
-        std::cout << "Constructor: " << m_name << '\n';
+        std::cout << "Constructor: " << m_val << '\n';
     }
 
     ~ScopeTest()
     {
-        std::cout << "Destructor:" << m_name << '\n';
+        std::cout << "Destructor:" << m_val << '\n';
     }
 
     void test()
     {
-        std::cout << "Val: " << m_name << '\n';
+        std::cout << "Val: " << m_val << '\n';
     }
 
     std::weak_ptr<ScopeTest> m_partner;
-
-private:
-    std::string m_name;
+    int m_val;
 };
 
 // Shared Pointer: besitzen ein Reference counting System
 void f1()
 {
-    auto t = std::make_shared<ScopeTest>("t1");
+    auto t = std::make_shared<ScopeTest>(1);
     t->test();
 
     std::cout << "Count: " << t.use_count() << '\n';
@@ -46,28 +44,29 @@ void f1()
 
 void f2()
 {
-    auto *t3 = new ScopeTest("t3");
+    auto *t3 = new ScopeTest(10);
     t3->test();
 }
 
 // Weak Pointer: erhöht den Reference Counter eines Shared Pointer NICHT
 void f3()
 {
-    auto t4 = std::make_shared<ScopeTest>("t4");
+    auto t4 = std::make_shared<ScopeTest>(11);
     std::cout << "Count t4: " << t4.use_count() << '\n';
-    auto t5 = std::make_shared<ScopeTest>("t5");
+    auto t5 = std::make_shared<ScopeTest>(12);
     std::cout << "Count t5: " << t5.use_count() << '\n';
 
     t4->m_partner = t5;
     std::cout << "Count t5: " << t5.use_count() << '\n';
     t5->m_partner = t4;
     std::cout << "Count t4: " << t4.use_count() << '\n';
-}
 
-void f4(std::shared_ptr<ScopeTest> t5)
-{
-    auto shared_t5_partner = t5->m_partner.lock();
-    shared_t5_partner->test();
+    if (!t4->m_partner.expired())
+    {
+        auto t4_partner_shard = t4->m_partner.lock();
+        std::cout << t4_partner_shard->m_val << std::endl;
+        std::cout << "Count t5: " << t5.use_count() << '\n';
+    }
 }
 
 int main()
