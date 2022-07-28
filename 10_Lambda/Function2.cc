a1769ca44b213ce10c3c6f559a4fa4f7e915b468
franneck94 <schaffranek.jan@web.de>
2022-07-28 15:51:33 +0200

    up

--- 10_Lambda/Function2.cc
+++ 10_Lambda/Function2.cc
@@ -4,18 +4,28 @@
 
 int main()
 {
+    const int threshold = 2;
     auto fns = std::vector<std::function<bool(const int)>>{
-        [](const int v) { return v > 2; },
-        [](const int v) { return v < 2; },
-        [](const int v) { return v == 2; },
-        [](const int v) { return v != 2; },
-        [](const int v) { return v >= 2; },
-        [](const int v) { return v <= 2; },
+        [&threshold](const int v) { return v > threshold; },
+        [&threshold](const int v) { return v < threshold; },
+        [&threshold](const int v) { return v == threshold; },
+        [&threshold](const int v) { return v != threshold; },
+        [&threshold](const int v) { return v >= threshold; },
+        [&threshold](const int v) { return v <= threshold; },
     };
+
+    // auto fns = std::vector<bool (*)(const int)>{
+    //     [](const int v) { return v > 2; },
+    //     [](const int v) { return v < 2; },
+    //     [](const int v) { return v == 2; },
+    //     [](const int v) { return v != 2; },
+    //     [](const int v) { return v >= 2; },
+    //     [](const int v) { return v <= 2; },
+    // };
 
     for (const auto &fn : fns)
     {
-        std::cout << std::boolalpha << fn(2) << '\n';
+        std::cout << std::boolalpha << fn(1) << '\n';
     }
 
     return 0;
