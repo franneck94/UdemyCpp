822f41c198f18dfe65be655c4a61f022798cf281
franneck94 <schaffranek.jan@web.de>
2022-08-22 17:11:47 +0200

    up

---
+++ 09_TemplatesOperator/FunctionTemplate.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+template <typename T>
+T max(const T &a, const T &b)
+{
+    if (a < b)
+    {
+        return b;
+    }
+    else
+    {
+        return a;
+    }
+}
+
+int main()
+{
+    std::cout << max(10, 11) << std::endl;
+    std::cout << max(10.0F, 11.0F) << std::endl;
+    std::cout << max(10.0, 11.0) << std::endl;
+
+    std::cout << max<int>(10, 11) << std::endl;
+    std::cout << max<float>(10, 11) << std::endl;
+    std::cout << max<double>(10, 11) << std::endl;
+
+    return 0;
+}
