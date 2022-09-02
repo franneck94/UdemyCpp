9fe557faae6fdfe8d0cf7c6a124fa45d3166d2ff
franneck94 <schaffranek.jan@web.de>
2022-09-02 14:55:23 +0200

    up

--- 05_String/Binary.cc
+++ 05_String/Binary.cc
@@ -1,30 +1,28 @@
 #include <bitset>
+#include <cstdint>
 #include <iostream>
 #include <string>
 
 int main()
 {
-    // Binary
-    std::bitset<8> my_byte1(std::string("00011101"));
-    std::bitset<8> my_byte2(std::string("10010101"));
+    auto my_byte1 = std::bitset<8>("00011101");
+    auto my_byte2 = std::bitset<8>("10010101");
 
-    std::cout << my_byte1 << std::endl;
-    std::cout << my_byte2 << std::endl;
+    std::cout << my_byte1 << '\n';
+    std::cout << my_byte2 << '\n';
 
-    std::cout << std::endl;
+    std::cout << "AND: " << (my_byte1 & my_byte2) << '\n';
+    std::cout << "OR: " << (my_byte1 | my_byte2) << '\n';
+    std::cout << "XOR: " << (my_byte1 ^ my_byte2) << '\n';
+    std::cout << "NEGATION: " << ~my_byte1 << '\n';
 
-    std::cout << "AND: " << (my_byte1 & my_byte2) << " (binary)" << std::endl;
-    std::cout << "OR: " << (my_byte1 | my_byte2) << " (binary)" << std::endl;
-    std::cout << "XOR: " << (my_byte1 ^ my_byte2) << " (binary)" << std::endl;
-    std::cout << "NEGATION: " << ~my_byte1 << " (binary)" << std::endl;
+    std::uint8_t v1 = 0x01;
+    std::uint8_t v2 = 0xFF;
 
-    // Hexa
-    unsigned char hex1 = 0x01; // 0000 0001
-    unsigned char hex2 = 0xff; // 1111 1111
-                               // 0000 0001
-
-    unsigned char hex3 = hex1 & hex2;
-    std::cout << "AND: " << static_cast<int>(hex3) << " (decimal)" << std::endl;
+    std::cout << "AND: " << (v1 & v2) << '\n';
+    std::cout << "OR: " << (v1 | v2) << '\n';
+    std::cout << "XOR: " << (v1 ^ v2) << '\n';
+    std::cout << "NEGATION: " << ~v1 << '\n';
 
     return 0;
 }
