0cbc673657a44ab7f858ec19edb96398982f30f1
franneck94 <schaffranek.jan@web.de>
2022-09-03 14:10:27 +0200

    up

--- 05_String/Binary.cc
+++ 05_String/Binary.cc
@@ -7,7 +7,6 @@ int main()
 {
     auto my_byte1 = std::bitset<8>("00011101");
     auto my_byte2 = std::bitset<8>("10010101");
-
     std::cout << my_byte1 << '\n';
     std::cout << my_byte2 << '\n';
 
@@ -18,6 +17,8 @@ int main()
 
     std::uint8_t v1 = 0x01;
     std::uint8_t v2 = 0xFF;
+    std::cout << static_cast<int>(v1) << '\n';
+    std::cout << static_cast<int>(v2) << '\n';
 
     std::cout << "AND: " << (v1 & v2) << '\n';
     std::cout << "OR: " << (v1 | v2) << '\n';
