7fb43e7528c18318141ecc9ef8243dfa4e0a3d6b
franneck94 <schaffranek.jan@web.de>
2022-09-09 15:38:07 +0200

    up

--- 04_Vector/VectorMethods2.cc
+++ 04_Vector/VectorMethods2.cc
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <numeric>
 #include <vector>
 
 void print_vector(const std::vector<int> &my_vector)
 {
-    for (std::size_t i = 0; i < my_vector.size(); i++)
+    for (std::size_t i = 0; i < my_vector.size(); ++i)
     {
-        std::cout << "Vec[" << i << "] = " << my_vector[i] << std::endl;
+        std::cout << my_vector[i] << std::endl;
     }
     std::cout << std::endl;
 }
+
 
 void print_vector_info(const std::vector<int> &my_vector)
 {
@@ -21,34 +21,42 @@ void print_vector_info(const std::vector<int> &my_vector)
 
 int main()
 {
-    std::vector<int> my_vector(10, 5);
-    std::iota(my_vector.begin(), my_vector.end(), 0);
-
+    auto my_vector = std::vector<int>{1, 2, 3, 4, 5};
     print_vector(my_vector);
     print_vector_info(my_vector);
 
-    int first = my_vector.front();
-    int first2 = my_vector[0];
-    std::cout << first << " " << first2 << std::endl;
+    auto p = my_vector.data();
+    std::cout << *p << std::endl;
+    p += 2;
+    std::cout << *p << std::endl << std::endl;
 
-    int last = my_vector.back();
-    int last2 = my_vector[my_vector.size() - 1];
-    std::cout << last << " " << last2 << std::endl;
+    auto &f = my_vector.front();
+    std::cout << f << std::endl;
+    f = -1;
+    std::cout << f << std::endl;
+    std::cout << my_vector[0] << std::endl << std::endl;
 
-    int *array = my_vector.data();
-    std::cout << *array << std::endl;
+    auto &b = my_vector.back();
+    std::cout << b << std::endl;
+    b = -5;
+    std::cout << b << std::endl;
+    std::cout << my_vector[my_vector.size() - 1] << std::endl << std::endl;
 
-    std::vector<int>::iterator begin = my_vector.begin();
-    std::vector<int>::iterator end = my_vector.end();
-
+    // [begin, end)
+    auto begin = my_vector.begin();
+    auto end = my_vector.end();
     std::cout << *begin << std::endl;
-    std::cout << *end << std::endl;
+    std::cout << *end << std::endl << std::endl;
 
-    begin++;
-    end--;
-
+    ++begin;
+    --end;
     std::cout << *begin << std::endl;
-    std::cout << *end << std::endl;
+    std::cout << *end << std::endl << std::endl;
+
+    for (auto it = my_vector.begin(); it != my_vector.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
 
     return 0;
 }
