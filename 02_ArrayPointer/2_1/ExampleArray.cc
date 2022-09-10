e834d4675893b4e146756b90edea9ed3826f76b6
franneck94 <schaffranek.jan@web.de>
2022-09-10 08:55:37 +0200

    up

--- 02_ArrayPointer/2_1/ExampleArray.cc
+++ 02_ArrayPointer/2_1/ExampleArray.cc
@@ -1,9 +1,11 @@
 #include <cstdint>
 #include <iostream>
 
+// Store in an array *array2* the reversed elements of *array1*.
+
 int main()
 {
-
+    std::int32_t array1[] = {1, 4, 7, 12};
 
     return 0;
 }
