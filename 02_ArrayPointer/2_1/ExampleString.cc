e00322d45156dec9d6e673e2ff42538f4e3339ca
franneck94 <schaffranek.jan@web.de>
2022-09-11 09:00:22 +0200

    up

---
+++ 02_ArrayPointer/2_1/ExampleString.cc
@@ -0,0 +1,12 @@
+#include <cstdint>
+#include <iostream>
+
+// Output the length of the char array (String)
+// The '\0' is not included in the length value
+
+int main()
+{
+    char array1[] = "Hello World!";
+
+    return 0;
+}
