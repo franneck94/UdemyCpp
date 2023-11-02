c77382d37adc676859d6dff9a5656ae70ac0de33
franneck94 <schaffranek.jan@web.de>
2023-11-02 20:53:19 +0100

    up

--- 02_Basics/2_1/Chars.cc
+++ 02_Basics/2_1/Chars.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 
-
 int main()
 {
     char letter_a = 'A';
--- 02_Basics/2_1/IntVsFloat.cc
+++ 02_Basics/2_1/IntVsFloat.cc
@@ -1,23 +1,21 @@
-
 #include <iostream>
 
 int main()
 {
-    float number1 = 10;
-    float number2 = 3;
-    float result = 0;
+    int my_value1 = 10;
+    int my_value2 = 3;
 
-    result = number1 * number2;
-    std::cout << "number1 * number2 = " << result << std::endl;
+    int my_result1 = my_value1 + my_value2;
+    std::cout << "addition: " << my_result1 << std::endl;
 
-    result = number1 + number2;
-    std::cout << "number1 + number2 = " << result << std::endl;
+    int my_result2 = my_value1 - my_value2;
+    std::cout << "subtraction: " << my_result2 << std::endl;
 
-    result = number1 - number2;
-    std::cout << "number1 - number2 = " << result << std::endl;
+    int my_result3 = my_value1 * my_value2;
+    std::cout << "multiplication: " << my_result3 << std::endl;
 
-    result = number1 / number2;
-    std::cout << "number1 / number2 = " << result << std::endl;
+    int my_result4 = my_value1 / my_value2;
+    std::cout << "division: " << my_result4 << std::endl;
 
     return 0;
 }
