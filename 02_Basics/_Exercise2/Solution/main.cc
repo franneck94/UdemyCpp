d2a1a7023f4163bb84edbe0bc3698e57a122ba13
franneck94 <schaffranek.jan@web.de>
2024-07-22 11:40:14 +0200

    fix

--- 02_Basics/_Exercise2/Solution/main.cc
+++ 02_Basics/_Exercise2/Solution/main.cc
@@ -6,19 +6,24 @@ void super_duper_example(const std::int32_t val)
     static constexpr std::int32_t reset_counter = 10;
     static std::int32_t counter = 0;
 
+    if (val == reset_counter)
+    {
+        counter = 0;
+        return;
+    }
+
     if (val % 2 == 0)
     {
         counter++;
-    }
-    else if (val == reset_counter)
-    {
-        counter = 0;
     }
 }
 
 int main()
 {
-
+    for (int i = 0; i < 11; i++)
+    {
+        super_duper_example(i);
+    }
 
     return 0;
 }
