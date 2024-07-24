c0532980de075b6f02334a5e2aa8978a1f5bf9b9
franneck94 <schaffranek.jan@web.de>
2024-07-24 14:59:48 +0200

    up

---
+++ 04_MoreBasics/GlobalStatic.cc
@@ -0,0 +1,32 @@
+#include <cstdint>
+#include <iostream>
+
+// static constexpr float PI = 3.14159; // PRIVATE
+
+namespace // anonym. namespace
+{
+constexpr float PI = 3.14159; // PRIVATE
+};
+
+float circle_area(const float radius)
+{
+    return PI * radius * radius;
+}
+
+constexpr std::uint32_t sum(const std::uint32_t n)
+{
+    if (n > 1)
+        return n + sum(n - 1);
+
+    return n;
+}
+
+int main()
+{
+    constexpr std::uint32_t n = 10;
+    constexpr std::uint32_t s = sum(n);
+
+    std::cout << "Sum from 0..10: " << s << std::endl;
+
+    return 0;
+}
